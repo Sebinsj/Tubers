49fddb94580cd3de1c81893f3929e562b81a34a4
Sebin Sonu Jose <sebinsonu7@gmail.com>
2023-12-11 20:52:47 +0530

    db changed to postgres

--- tubers/settings.py
+++ tubers/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'tubers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tubers',
+        'USER':'postgres',
+        'PASSWORD':'test1234',
+        "HOST":'localhost'
     }
 }
 
