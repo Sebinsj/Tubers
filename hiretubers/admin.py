a422ac4ea2ef46f800da1929f0973372df946ae4
Sebin Sonu Jose <sebinsonu7@gmail.com>
2023-12-23 00:29:24 +0530

    backend assignment fixed

--- hiretubers/admin.py
+++ hiretubers/admin.py
@@ -4,8 +4,8 @@ from .models import Hiretuber
 # Register your models here.
 class HiretuberAdmin(admin.ModelAdmin):
     
-    list_display=('first_name','email',)
-    list_display_links=("first_name",'email',)
+    list_display=('first_name','email',"tuber_name")
+    list_display_links=("first_name",'email',"tuber_name")
     search_fields=("first_name","last_name")
     list_filter=()
 
